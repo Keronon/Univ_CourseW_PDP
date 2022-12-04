03b5c470a9d24daf89b8704e75bd86f9d797e967
Keronon S <59052298+Keronon@users.noreply.github.com>
2022-12-04 20:45:27 +0300

    04.12.22 Crypto module added

---
+++ Email_Service/Crypto_module.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text.Json;
+
+namespace Email_Service
+{
+    internal class Crypto_module
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions { WriteIndented = true };
+
+        /*
+        Симметричный алгоритм шифрования – AES.
+        Ассиметричный алгоритм шифрования для шифрования ключей симметричного алгоритма – RSA.
+        Алгоритм хеширования – MD5.
+        Ассиметричный алгоритм шифрования для ЭЦП – RSA.
+
+        Для стандартной инициализации RSAProvider'а в .Net :
+        KeySize = 256, BlockSize = 128, Mode = CipherMode.CBC
+        */
+
+        private void EXAMPLE_Load(object sender, EventArgs e)
+        {
+            RSA_Core[] keys_list;
+            RSA_Core[] signs_list;
+
+            using (FileStream file = new FileStream("core_crypt.keys", FileMode.OpenOrCreate))
+                if (file.Length > 0) keys_list = JsonSerializer.Deserialize<RSA_Core[]>(file);
+            using (FileStream file = new FileStream("core_sign.keys", FileMode.OpenOrCreate))
+                if (file.Length > 0) signs_list = JsonSerializer.Deserialize<RSA_Core[]>(file);
+        }
+        private void EXAMPLE_FormClosed(object sender, EventArgs e)
+        {
+            RSA_Core[] keys_list  = new RSA_Core[1];
+            RSA_Core[] signs_list = new RSA_Core[1];
+
+            using (FileStream file = new FileStream("core_crypt.keys", FileMode.OpenOrCreate))
+                JsonSerializer.Serialize(file, keys_list, options);
+            using (FileStream file = new FileStream("core_sign.keys", FileMode.OpenOrCreate))
+                JsonSerializer.Serialize(file, signs_list, options);
+        }
+
+        private void EXAMPLE_encrypt_Click(object sender, EventArgs e)
+        {
+            RSA_Core[] keys_list = new RSA_Core[1];
+            if (keys_list[0].Name == "") { Console.WriteLine("ПЕРЕД ШИФРОВАНИЕМ УКАЖИТЕ ИМЯ ШИФРОВОЙ ГРУППЫ\n\n(первый выпадающий список)"); return; }
+
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            RSA_Core core;
+
+            if ((core = Get_core(keys_list, "core name")) == null)
+            {
+                core = new RSA_Core(keys_list[0].Name, RSA.ToXmlString(true), RSA.ToXmlString(false));
+                // keys_list.Add(core);
+            }
+
+            // if (DIALOG_file.ShowDialog() == DialogResult.OK)
+            {
+                string file = "FileName";
+                RSAEncryptFile(file, file.Insert(file.LastIndexOf('.'), "(!)"), core.Public_key);
+
+                Console.WriteLine("зашифровано");
+            }
+        }
+        private void EXAMPLE_decrypt_Click(object sender, EventArgs e)
+        {
+            RSA_Core[] keys_list = new RSA_Core[1];
+            if (keys_list[0].Name == "") { Console.WriteLine("ПЕРЕД РАСШИФРОВЫВАНИЕМ УКАЖИТЕ ИМЯ ШИФРОВОЙ ГРУППЫ\n\n(первый выпадающий список)"); return; }
+
+            RSA_Core core;
+
+            if ((core = Get_core(keys_list, "core name")) == null)
+            {
+                Console.WriteLine("У ВАС НЕТ ДАННЫХ ДЛЯ ДЕШИФРОВКИ УКАЗАННОЙ ШИФРОВОЙ ГРУППЫ\n\n(первый выпадающий список)"); return;
+            }
+
+            // if (DIALOG_file.ShowDialog() == DialogResult.OK)
+            {
+                string file = "FileName";
+                int index = file.LastIndexOf("!");
+                RSADecryptFile(file, file.Remove(index, 1).Insert(index, "+"), core.Private_key);
+
+                Console.WriteLine("расшифровано");
+            }
+        }
+
+        private void EXAMPLE_put_sign_Click(object sender, EventArgs e)
+        {
+            RSA_Core[] signs_list = new RSA_Core[1];
+            if (signs_list[0].Name == "") { Console.WriteLine("ПЕРЕД ПОДПИСЫВАНИЕМ УКАЖИТЕ ИМЯ ПОДПИСИ\n\n(второй выпадающий список)"); return; }
+
+            RSACryptoServiceProvider RSA = new RSACryptoServiceProvider();
+            RSA_Core core;
+
+            if ((core = Get_core(signs_list, "sign name")) == null)
+            {
+                core = new RSA_Core(signs_list[0].Name, RSA.ToXmlString(true), RSA.ToXmlString(false));
+                // COMBO_core_sign.Items.Add(core);
+            }
+
+            // if (DIALOG_file.ShowDialog() == DialogResult.OK)
+            {
+                string file = "FileName";
+                SignFile(file, file.Insert(file.LastIndexOf('.'), "($)"), core.Private_key);
+
+                Console.WriteLine("подписано");
+            }
+        }
+        private void EXAMPLE_check_sign_Click(object sender, EventArgs e)
+        {
+            RSA_Core[] signs_list = new RSA_Core[1];
+            if (signs_list[0].Name == "") { Console.WriteLine("ПЕРЕД ПРОВЕРКОЙ ПОДПИСИ УКАЖИТЕ ИМЯ ПОДПИСИ\n\n(второй выпадающий список)"); return; }
+
+            RSA_Core core;
+
+            if ((core = Get_core(signs_list, "sign name")) == null)
+            {
+                Console.WriteLine("У ВАС НЕТ ДАННЫХ ДЛЯ ПРОВЕРКИ УКАЗАННОЙ ПОДПИСИ\n\n(второй выпадающий список)"); return;
+            }
+
+            // if (DIALOG_file.ShowDialog() == DialogResult.OK)
+            {
+                string file = "FileName";
+                if (CheckFile(file, core.Private_key))
+                    Console.WriteLine("подпись подтверждена");
+                else
+                    Console.WriteLine("целостность файла нарушена");
+            }
+        }
+
+        private RSA_Core Get_core(RSA_Core[] cores, string name)
+        {
+            foreach (RSA_Core core_crypt in cores)
+            {
+                if (core_crypt.Name == name) return core_crypt;
+            }
+            return null;
+        }
+
+        public static void RSAEncryptFile(string standart_file, string encrypted_file, string public_key)
+        {
+            using (var input = File.OpenRead(standart_file))
+            using (var output = File.Create(encrypted_file))
+            using (var aes = Aes.Create())
+            {
+                output.Write(aes.IV, 0, aes.IV.Length);
+                byte[] encryptedAesKey = RSAEncrypt(aes.Key, public_key);
+                byte[] lengthBytes = BitConverter.GetBytes(encryptedAesKey.Length);
+                output.Write(lengthBytes, 0, lengthBytes.Length);
+                output.Write(encryptedAesKey, 0, encryptedAesKey.Length);
+                using (var cs = new CryptoStream(output, aes.CreateEncryptor(), CryptoStreamMode.Write)) input.CopyTo(cs);
+            }
+        }
+
+        public static void RSADecryptFile(string encrypted_file, string standart_file, string private_key)
+        {
+            using (var input = File.OpenRead(encrypted_file))
+            using (var output = File.Create(standart_file))
+            using (var aes = Aes.Create())
+            {
+                byte[] iv = new byte[aes.BlockSize / 8];
+                input.Read(iv, 0, iv.Length);
+                aes.IV = iv;
+                byte[] lengthBytes = new byte[sizeof(int)];
+                input.Read(lengthBytes, 0, lengthBytes.Length);
+                int length = BitConverter.ToInt32(lengthBytes, 0);
+                byte[] encryptedAesKey = new byte[length];
+                input.Read(encryptedAesKey, 0, encryptedAesKey.Length);
+                aes.Key = RSADecrypt(encryptedAesKey, private_key);
+                using (var cs = new CryptoStream(input, aes.CreateDecryptor(), CryptoStreamMode.Read)) cs.CopyTo(output);
+            }
+        }
+
+        public static byte[] RSAEncrypt(byte[] data, string public_key)
+        {
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.FromXmlString(public_key);
+                return rsa.Encrypt(data, true);
+            }
+        }
+
+        public static byte[] RSADecrypt(byte[] data, string private_key)
+        {
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.FromXmlString(private_key);
+                return rsa.Decrypt(data, true);
+            }
+        }
+
+        public void SignFile(string standart_file, string signed_file, string private_key)
+        {
+            using (var input = File.OpenRead(standart_file))
+            using (var output = File.Create(signed_file))
+            using (var rsa = new RSACryptoServiceProvider(256))
+            {
+                rsa.FromXmlString(private_key);
+                byte[] sign = rsa.SignData(input, MD5.Create());
+
+                input.Position = 0;
+                input.CopyTo(output);
+                output.Write(sign, 0, sign.Length);
+            }
+        }
+
+        public bool CheckFile(string file, string private_key)
+        {
+            using (var file_stream = File.OpenRead(file))
+            {
+                byte[] data = new byte[file_stream.Length - 128];
+                file_stream.Read(data, 0, data.Length);
+                byte[] sign = new byte[128];
+                file_stream.Read(sign, 0, sign.Length);
+
+                using (var rsa = new RSACryptoServiceProvider(256))
+                {
+                    rsa.FromXmlString(private_key);
+                    return rsa.VerifyData(data, MD5.Create(), sign);
+                }
+            }
+        }
+    }
+
+    public class RSA_Core
+    {
+        public string Name { get; }
+        public string Private_key { get; }
+        public string Public_key { get; }
+
+        public RSA_Core(string name, string private_key, string public_key)
+        {
+            Name = name;
+            Private_key = private_key;
+            Public_key = public_key;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- Email_Service/FORM_Main.Designer.cs
+++ Email_Service/FORM_Main.Designer.cs
@@ -43,6 +43,8 @@
             this.PIC_avatar = new System.Windows.Forms.PictureBox();
             this.MENU_ITEM_update = new System.Windows.Forms.ToolStripMenuItem();
             this.MENU_ITEM_logout = new System.Windows.Forms.ToolStripMenuItem();
+            this.LIST_appended = new System.Windows.Forms.ListBox();
+            this.NOTIFY_ICON = new System.Windows.Forms.NotifyIcon(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.SPLIT_container)).BeginInit();
             this.SPLIT_container.Panel1.SuspendLayout();
             this.SPLIT_container.Panel2.SuspendLayout();
@@ -54,7 +56,9 @@
             // 
             // SPLIT_container
             // 
-            this.SPLIT_container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SPLIT_container.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.SPLIT_container.Location = new System.Drawing.Point(0, 0);
             this.SPLIT_container.Name = "SPLIT_container";
             // 
@@ -65,6 +69,7 @@
             // 
             // SPLIT_container.Panel2
             // 
+            this.SPLIT_container.Panel2.Controls.Add(this.LIST_appended);
             this.SPLIT_container.Panel2.Controls.Add(this.TEXT_message);
             this.SPLIT_container.Size = new System.Drawing.Size(884, 561);
             this.SPLIT_container.SplitterDistance = 294;
@@ -72,13 +77,13 @@
             // 
             // FLOW_PANEL_chains
             // 
-            this.FLOW_PANEL_chains.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.FLOW_PANEL_chains.AutoScroll = true;
+            this.FLOW_PANEL_chains.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FLOW_PANEL_chains.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.FLOW_PANEL_chains.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.FLOW_PANEL_chains.Location = new System.Drawing.Point(3, 99);
+            this.FLOW_PANEL_chains.Location = new System.Drawing.Point(0, 102);
             this.FLOW_PANEL_chains.Name = "FLOW_PANEL_chains";
-            this.FLOW_PANEL_chains.Size = new System.Drawing.Size(288, 459);
+            this.FLOW_PANEL_chains.Size = new System.Drawing.Size(294, 459);
             this.FLOW_PANEL_chains.TabIndex = 1;
             // 
             // CONTEXT_MENU_account
@@ -98,9 +103,11 @@
             // 
             // TEXT_message
             // 
-            this.TEXT_message.Location = new System.Drawing.Point(3, 3);
+            this.TEXT_message.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TEXT_message.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TEXT_message.Location = new System.Drawing.Point(0, 0);
             this.TEXT_message.Name = "TEXT_message";
-            this.TEXT_message.Size = new System.Drawing.Size(580, 555);
+            this.TEXT_message.Size = new System.Drawing.Size(586, 418);
             this.TEXT_message.TabIndex = 0;
             this.TEXT_message.Text = "";
             // 
@@ -125,13 +132,11 @@
             // 
             // MENU_ITEM_email
             // 
-            this.MENU_ITEM_email.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.MENU_ITEM_email.Name = "MENU_ITEM_email";
             this.MENU_ITEM_email.Size = new System.Drawing.Size(100, 23);
             // 
             // MENU_ITEM_password
             // 
-            this.MENU_ITEM_password.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.MENU_ITEM_password.Name = "MENU_ITEM_password";
             this.MENU_ITEM_password.Size = new System.Drawing.Size(100, 23);
             // 
@@ -145,13 +150,14 @@
             // 
             // PIC_avatar
             // 
-            this.PIC_avatar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.PIC_avatar.BackColor = System.Drawing.Color.LightCyan;
+            this.PIC_avatar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.PIC_avatar.ContextMenuStrip = this.CONTEXT_MENU_account;
-            this.PIC_avatar.Location = new System.Drawing.Point(102, 3);
+            this.PIC_avatar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PIC_avatar.Location = new System.Drawing.Point(0, 0);
             this.PIC_avatar.Name = "PIC_avatar";
-            this.PIC_avatar.Size = new System.Drawing.Size(90, 90);
+            this.PIC_avatar.Size = new System.Drawing.Size(294, 96);
+            this.PIC_avatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.PIC_avatar.TabIndex = 0;
             this.PIC_avatar.TabStop = false;
             this.PIC_avatar.MouseEnter += new System.EventHandler(this.PIC_avatar_MouseEnter);
@@ -173,6 +179,24 @@
             this.MENU_ITEM_logout.Size = new System.Drawing.Size(177, 22);
             this.MENU_ITEM_logout.Text = "Выйти из профиля";
             // 
+            // LIST_appended
+            // 
+            this.LIST_appended.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LIST_appended.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.LIST_appended.ForeColor = System.Drawing.Color.DarkBlue;
+            this.LIST_appended.FormattingEnabled = true;
+            this.LIST_appended.ItemHeight = 15;
+            this.LIST_appended.Location = new System.Drawing.Point(0, 424);
+            this.LIST_appended.Name = "LIST_appended";
+            this.LIST_appended.Size = new System.Drawing.Size(586, 137);
+            this.LIST_appended.TabIndex = 1;
+            // 
+            // NOTIFY_ICON
+            // 
+            this.NOTIFY_ICON.Icon = ((System.Drawing.Icon)(resources.GetObject("NOTIFY_ICON.Icon")));
+            this.NOTIFY_ICON.Text = "Email Service";
+            this.NOTIFY_ICON.Visible = true;
+            // 
             // FORM_Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -181,9 +205,12 @@
             this.Controls.Add(this.SPLIT_container);
             this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.ForeColor = System.Drawing.Color.DarkBlue;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
             this.Name = "FORM_Main";
+            this.ShowInTaskbar = false;
             this.Text = "Email Service";
             this.SPLIT_container.Panel1.ResumeLayout(false);
             this.SPLIT_container.Panel2.ResumeLayout(false);
@@ -211,6 +238,8 @@
         private System.Windows.Forms.ToolStripTextBox MENU_ITEM_email;
         private System.Windows.Forms.ToolStripTextBox MENU_ITEM_password;
         private System.Windows.Forms.ToolStripMenuItem MENU_ITEM_apply;
+        private System.Windows.Forms.ListBox LIST_appended;
+        private System.Windows.Forms.NotifyIcon NOTIFY_ICON;
     }
 }
 
